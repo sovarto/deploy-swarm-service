6bf93ad5decfa09d93aa05ea0eb313266b19b2f6
Daniel Hilgarth <d.hilgarth@sovarto.com>
2024-05-23 22:31:09 +0200

    feat: Initial commit

---
+++ src/main.ts
@@ -0,0 +1,24 @@
+import * as core from '@actions/core';
+import { deploy } from '@sovarto/pipelines-deploy-service-core';
+
+/**
+ * The main function for the action.
+ * @returns {Promise<void>} Resolves when the action is complete.
+ */
+export async function run(): Promise<void> {
+    try {
+        const servicesDefinitionFile = core.getInput('services-definition', { required: true });
+        const stackName = core.getInput('stack-name', { required: true });
+        const remoteStateAccessToken = core.getInput('remote-state-access-token', { required: true });
+
+        await deploy(stackName, remoteStateAccessToken, servicesDefinitionFile);
+    } catch (error) {
+        if (error instanceof Error) {
+            core.setFailed(error.message);
+        } else {
+            core.setFailed(`Unknown error of type '${ typeof error }${ typeof error === 'object'
+                                                                       ? ` / ${ error!.constructor.name }`
+                                                                       : '' }' occurred:\n\n${error}`);
+        }
+    }
+}
